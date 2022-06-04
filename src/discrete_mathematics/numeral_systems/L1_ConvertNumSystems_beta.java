35e17cbb9051b169a4040aff8a9305efa56ca123
Trayan Ivanov <ivanov.trayan@outlook.com>
2022-06-04 21:48:52 +0300

    Reformatted packages.

--- src/discrete_mathematics/numeral_systems/L1_ConvertNumSystems_beta.java
+++ src/discrete_mathematics/numeral_systems/L1_ConvertNumSystems_beta.java
@@ -1,4 +1,4 @@
-package helpers;
+package numeral_systems;
 
 import java.math.BigInteger;
 import java.util.Scanner;
--- src/discrete_mathematics/quadratic_equations/L2_2_BiQuadraticEquations.java
+++ src/discrete_mathematics/quadratic_equations/L2_2_BiQuadraticEquations.java
@@ -1,11 +1,11 @@
-package helpers;
+package quadratic_equations;
 
 import java.util.Scanner;
 
-public class L2_1_QuadraticEquations {
+public class L2_2_BiQuadraticEquations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("(a)x^2 + (b)x + (c) = 0");
+        System.out.println("(a)x^4 + (b)x^2 + (c) = 0");
         System.out.print("a = ");
         int a = Integer.parseInt(scanner.nextLine());
 
@@ -26,16 +26,17 @@ public class L2_1_QuadraticEquations {
             System.out.println("No solutions exist");
         } else if (discriminant == 0) {
             double x = (-b * 1.0) / (2 * a);
-            System.out.printf("The only solution (root) is %f%n", x);
+            double y = Math.sqrt(x);
+            System.out.printf("The solution is SQRT(%.0f) = [+/-]%f%n", x, y);
+
         } else {
             double sqrtD = Math.sqrt(discriminant);
 
             double x1 = (-b + sqrtD) / (2 * a);
             double x2 = (-b - sqrtD) / (2 * a);
-            System.out.printf("The solutions are %.0f and %.0f%n", x1, x2);
-            String signX1 = (x1 < 0) ? "-" : "";
-            String signX2 = (x2 < 0) ? "-" : "";
-            System.out.printf("The solutions in sqrt are: %sSQRT(%.0f) and %sSQRT(%.0f)%n", signX1, x1 * x1, signX2, x2 * x2);
+
+            System.out.printf("The solutions are: SQRT(%.0f) = [+/-]%.2f  && SQRT(%.0f) = [+/-]%.2f%n", x1, Math.sqrt(x1), x2, Math.sqrt(x2));
+
         }
     }
 }

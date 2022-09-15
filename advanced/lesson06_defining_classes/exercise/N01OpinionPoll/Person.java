f6b16dbc7dac54781100c31c51d90ec25fd3d14d
Trayan Ivanov <101820581+thrako@users.noreply.github.com>
2022-09-15 11:21:11 +0300

    Defining Classes / Exercise / Task 1

---
+++ advanced/lesson06_defining_classes/exercise/N01OpinionPoll/Person.java
@@ -0,0 +1,24 @@
+package lesson06_defining_classes.exercise.N01OpinionPoll;
+
+class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %d", this.name, this.age);
+    }
+}

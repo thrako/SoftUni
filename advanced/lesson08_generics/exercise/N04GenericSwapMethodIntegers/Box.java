ecbeabe504c743c86f93ec90692a8ed6777148c4
Trayan Ivanov <101820581+thrako@users.noreply.github.com>
2022-09-25 07:47:47 +0300

    Generics / Exercise / Task 4

---
+++ advanced/lesson08_generics/exercise/N04GenericSwapMethodIntegers/Box.java
@@ -0,0 +1,14 @@
+package lesson08_generics.exercise.N04GenericSwapMethodIntegers;
+
+public class Box<T> {
+    private final T element;
+
+    public Box(T element) {
+        this.element = element;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s", element.getClass().toString().substring(6), element);
+    }
+}

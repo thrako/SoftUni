51af4436314740e9967415c43098204c96f53991
Trayan Ivanov <101820581+thrako@users.noreply.github.com>
2022-10-01 12:11:40 +0300

    Create N15_BasicSelectAllFieldsAndOrderThem.sql

---
+++ JavaDB/MySQL/L01_DataDefinitionAndDataTypes/Exercise/N15_BasicSelectAllFieldsAndOrderThem.sql
@@ -0,0 +1,10 @@
+### Task 15. Basic Select All Fields and Order Them
+
+SELECT * FROM towns 
+ORDER BY `name` ASC;
+
+SELECT * FROM departments 
+ORDER BY `name` ASC;
+
+SELECT * FROM employees 
+ORDER BY `salary` DESC;

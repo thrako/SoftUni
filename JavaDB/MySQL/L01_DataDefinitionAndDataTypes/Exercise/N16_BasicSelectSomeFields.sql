4a335d195fcdfc330932c662b4b4f8c5d3135014
Trayan Ivanov <101820581+thrako@users.noreply.github.com>
2022-10-01 12:11:43 +0300

    Create N16_BasicSelectSomeFields.sql

---
+++ JavaDB/MySQL/L01_DataDefinitionAndDataTypes/Exercise/N16_BasicSelectSomeFields.sql
@@ -0,0 +1,10 @@
+### Task 16. Basic Select Some Fields
+
+SELECT `name` FROM towns 
+ORDER BY `name` ASC;
+
+SELECT `name` FROM departments 
+ORDER BY `name` ASC;
+
+SELECT `first_name`,`last_name`,`job_title`,`salary` FROM employees 
+ORDER BY `salary` DESC;

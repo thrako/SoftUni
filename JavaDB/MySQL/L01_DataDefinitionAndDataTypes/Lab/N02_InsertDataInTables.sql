e62b48796dafc3a7cee82253971c503b0e6daf06
Trayan Ivanov <101820581+thrako@users.noreply.github.com>
2022-10-06 17:02:36 +0300

    Create N02_InsertDataInTables.sql

---
+++ JavaDB/MySQL/L01_DataDefinitionAndDataTypes/Lab/N02_InsertDataInTables.sql
@@ -0,0 +1,7 @@
+### Task 02. Insert Data in Tables
+
+INSERT INTO employees 
+			(`first_name`, `last_name`)
+     VALUES ('testFirst1', 'testLast1'),
+            ('testFirst2', 'testLast2'),
+            ('testFirst3', 'testLast3');

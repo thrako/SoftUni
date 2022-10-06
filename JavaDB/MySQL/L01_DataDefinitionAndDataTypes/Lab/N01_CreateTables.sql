9898f76a49b917091e417a7637122ce528e5b236
Trayan Ivanov <101820581+thrako@users.noreply.github.com>
2022-10-06 17:02:27 +0300

    Create N01_CreateTables.sql

---
+++ JavaDB/MySQL/L01_DataDefinitionAndDataTypes/Lab/N01_CreateTables.sql
@@ -0,0 +1,18 @@
+### Task 01. Create Tables
+
+CREATE TABLE employees (
+	         `id` INT PRIMARY KEY AUTO_INCREMENT,
+	 `first_name` VARCHAR(50) NOT NULL, 
+	  `last_name` VARCHAR(50) NOT NULL  
+);
+
+CREATE TABLE categories (
+	         `id` INT PRIMARY KEY AUTO_INCREMENT,
+           `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE products (
+	         `id` INT PRIMARY KEY AUTO_INCREMENT,
+           `name` VARCHAR(50) NOT NULL,
+    `category_id` INT NOT NULL
+);

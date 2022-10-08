0b107414f444b14e47caa9ccc65923b723f27c02
Trayan Ivanov <101820581+thrako@users.noreply.github.com>
2022-10-08 10:29:27 +0300

    Create N05_SelectEmployeesByMultipleFilters.sql

---
+++ JavaDB/MySQL/L02_BasicCRUD/Lab/N05_SelectEmployeesByMultipleFilters.sql
@@ -0,0 +1,7 @@
+### Task 05. Select Employees by Multiple Filters
+
+  SELECT `id`, `first_name`, `last_name`, `job_title`, `department_id`, `salary`
+    FROM  employees
+   WHERE `department_id` = 4 AND
+		 `salary` >= 1000
+ORDER BY `id`;

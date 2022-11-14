848e7ed94219c59f0f7adaea104e62de2d062947
Trayan Ivanov <101820581+thrako@users.noreply.github.com>
2022-11-14 15:44:37 +0200

    Inheritance / Exercise / Task 1

---
+++ JavaAdvanced/JavaOOP/lesson03_inheritance/exercise/n01_person/Child.java
@@ -0,0 +1,7 @@
+package lesson03_inheritance.exercise.n01_person;
+
+public class Child extends Person {
+    public Child(String name, int age) {
+        super(name, age);
+    }
+}
---
+++ JavaAdvanced/JavaOOP/lesson03_inheritance/exercise/n01_person/Person.java
@@ -0,0 +1,27 @@
+package lesson03_inheritance.exercise.n01_person;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.setName(name);
+        this.setAge(age);
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}

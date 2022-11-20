3ce48ff38daaac49addc01085be6ee185b26dedf
Trayan Ivanov <101820581+thrako@users.noreply.github.com>
2022-11-20 17:32:10 +0200

    Interfaces / Lab / Task 6

---
+++ JavaAdvanced/JavaOOP/lesson04_interfaces/lab/n06_ferarri/Car.java
@@ -0,0 +1,9 @@
+package lesson04_interfaces.lab.n06_ferarri;
+
+public interface Car {
+
+    String brakes();
+
+    String gas();
+
+}
---
+++ JavaAdvanced/JavaOOP/lesson04_interfaces/lab/n06_ferarri/Main.java
@@ -0,0 +1,13 @@
+package lesson04_interfaces.lab.n06_ferarri;
+
+import lesson04_interfaces.lab.n06_ferarri.Ferrari;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new Ferrari(new Scanner(System.in).nextLine()));
+    }
+
+}
